41382785234d6defb49521375ae41ecb27f9245c
ThaiDuong05 <duongcoke5@gmail.com>
2025-02-21 16:12:12 +0700

    Update_Btap21/2

--- PTPMQL/FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ PTPMQL/FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eab5556f17ec265659acef5ee8ed4d2425cadd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03adfc26cc36951c0cc286b7fecdb931b2e84c9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
